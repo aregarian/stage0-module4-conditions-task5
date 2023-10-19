88b6989ed2d186d313aef0d139ebf882d5b42020
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-19 07:23:25 +0300

    adfgsdvbaefbaweg

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int r;
+        if (year % 400 == 0){
+            r = 1;
+        }else if (year % 4 != 0 || year % 100 == 0) {
+            r = 0;
+        } else {
+            r = 1;
+        }
+
+        switch (r) {
+            case (1):
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
